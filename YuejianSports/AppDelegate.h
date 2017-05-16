edbc13eaf6b488c6f9c276a453f0c6320493ac08
wang yan <admin@wangdemacbook-air.local>
2017-05-16 10:34:48 +0800

    Initial Commit

---
+++ YuejianSports/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YuejianSports
+//
+//  Created by wang yan on 17/5/16.
+//  Copyright © 2017年 wang yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
