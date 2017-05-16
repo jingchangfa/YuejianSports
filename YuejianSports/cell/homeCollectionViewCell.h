da83d54ba9a121e7bae43ed08aa69529bd5ae030
wang yan <admin@wangdemacbook-air.local>
2017-05-16 17:17:30 +0800

    a

---
+++ YuejianSports/cell/homeCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  homeCollectionViewCell.h
+//  YuejianSports
+//
+//  Created by wang yan on 17/5/16.
+//  Copyright © 2017年 wang yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface homeCollectionViewCell : UICollectionViewCell
+@property(nonatomic,strong)UIImageView *image;
+@property(nonatomic,strong)UILabel  *label;
+@end
